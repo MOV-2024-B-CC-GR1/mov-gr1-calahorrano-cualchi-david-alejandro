a6e6269ee92a84add37a679b9454a4226a9fc622
DevTDavicho <developer2023Davicho@outlook.es>
2024-10-24 09:27:24 -0500

    Initial setup for Kotlin project

---
+++ 00-Kotlin/kotlin-ejemplo/src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+package org.example
+
+fun main() {
+    println("Hello World!")
+
+    // INMUTABLES (No se RE ASIGNA "=")
+    val inmutable: String = "David";
+
+    // inmutable = "David" // Error!
+
+    //MUTABLES
+    var mutable: String = "Alejandro";
+    mutable = "David"; //Ok
+
+    //VAL > VAR
+}
