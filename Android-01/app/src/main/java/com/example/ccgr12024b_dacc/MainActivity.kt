3165d22a5d1670a233f97a5bd3c11d71a69d389a
DevTDavicho <developer2023Davicho@outlook.es>
2025-01-16 11:23:38 -0500

    Added explicit and implicit intent functionality with result handling and contact validation

--- Android-01/app/src/main/java/com/example/ccgr12024b_dacc/MainActivity.kt
+++ Android-01/app/src/main/java/com/example/ccgr12024b_dacc/MainActivity.kt
@@ -1,14 +1,64 @@
 package com.example.ccgr12024b_dacc
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    fun mostrarSnackbar(texto: String){
+        val snack = Snackbar.make(
+            findViewById(R.id.cl_ciclo_vida),
+            texto,
+            Snackbar.LENGTH_INDEFINITE
+        )
+        snack.show()
+    }
+
+    val callbackContenidoIntentExplicito = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+            result ->
+        if(result.resultCode == Activity.RESULT_OK){
+            if(result.data != null){
+                val data = result.data?.getStringExtra("nombreModificado")
+                mostrarSnackbar("$data")
+            }
+        }
+    }
+
+    val callbackContenidoIntentImplicito = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){
+        result -> if (result.resultCode == Activity.RESULT_OK){
+            if (result.data != null){
+                // Validación de contactos
+                if (result.data!!.data != null){
+                    var uri: Uri = result.data!!.data!!
+                    val cursor = contentResolver.query(
+                        uri, null, null, null, null, null
+                    )
+                    cursor?.moveToFirst()
+                    val indiceTelefono = cursor?.getColumnIndex(
+                        ContactsContract.CommonDataKinds.Phone.NUMBER
+                    )
+
+                    val telefono = cursor?.getString(indiceTelefono!!)
+                    cursor?.close()
+                    mostrarSnackbar("Teléfono: $telefono")
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
