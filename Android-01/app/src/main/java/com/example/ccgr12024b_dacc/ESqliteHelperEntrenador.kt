d577aec0fe7e7cd5ede5a19f729de5a4cba2579b
DevTDavicho <developer2023Davicho@outlook.es>
2025-01-17 09:05:04 -0500

    Added SQLiteOpenHelper implementation for Entrenador table creation

--- Android-01/app/src/main/java/com/example/ccgr12024b_dacc/ESqliteHelperEntrenador.kt
+++ Android-01/app/src/main/java/com/example/ccgr12024b_dacc/ESqliteHelperEntrenador.kt
@@ -1,4 +1,31 @@
 package com.example.ccgr12024b_dacc
 
-class ESqliteHelperEntrenador {
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class ESqliteHelperEntrenador(
+    contexto: Context? //this
+): SQLiteOpenHelper(
+    contexto,
+    "moviles",
+    null,
+    1
+) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        val scriptSQLCrearTablaEntrenador =
+            """
+                CREATE TABLE ENTRENADOR(
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    nombre VARCHAR(50),
+                    descripcion VARCHAR(50)
+                )
+            """.trimIndent()
+        db?.execSQL(scriptSQLCrearTablaEntrenador)
+    }
+    override fun onUpgrade(
+        db: SQLiteDatabase?,
+        p1: Int,
+        p2: Int
+    ) {}
 }
