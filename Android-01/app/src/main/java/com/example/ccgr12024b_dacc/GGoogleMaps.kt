b6417a3c5344dcaa980e4fe0e6c2dc95022d11e0
DevTDavicho <developer2023Davicho@outlook.es>
2025-01-23 10:01:38 -0500

    Added new functions

--- Android-01/app/src/main/java/com/example/ccgr12024b_dacc/GGoogleMaps.kt
+++ Android-01/app/src/main/java/com/example/ccgr12024b_dacc/GGoogleMaps.kt
@@ -1,12 +1,29 @@
 package com.example.ccgr12024b_dacc
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 
 class GGoogleMaps : AppCompatActivity() {
+    private lateinit var mapa: GoogleMap
+    var permisos = false
+    val nombrePermisoFine = android.Manifest.permission.ACCESS_FINE_LOCATION
+    val nombrePermisoCoarse = android.Manifest.permission.ACCESS_COARSE_LOCATION
+    // val CODIGO_PERMISO_LOCALIZACION = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +33,69 @@ class GGoogleMaps : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        solicitarPermisos()
+        iniciarGoogleMaps()
+    }
+
+    fun tengoPermisos(): Boolean{
+        val contexto = applicationContext
+        val permisoFine = ContextCompat.checkSelfPermission(contexto, nombrePermisoFine)
+        val permisoCoarse = ContextCompat.checkSelfPermission(contexto, nombrePermisoCoarse)
+        val tienePermisoFine = permisoFine == PackageManager.PERMISSION_GRANTED &&
+                permisoCoarse == PackageManager.PERMISSION_GRANTED
+
+        permisos = tienePermisoFine
+        return permisos
+    }
+
+    fun solicitarPermisos() {
+        if(!tengoPermisos()){
+            ActivityCompat.requestPermissions(
+                this, arrayOf(nombrePermisoFine, nombrePermisoCoarse), 1
+            )
+        }
+    }
+
+    fun iniciarGoogleMaps() {
+        val fragmentoMapa = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        fragmentoMapa.getMapAsync{
+            googleMap -> with(googleMap){
+                mapa = googleMap
+                establecerConfiguracionMapa()
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    fun establecerConfiguracionMapa() {
+        with(mapa){
+            if(tengoPermisos()) {
+                mapa.isMyLocationEnabled = true
+                uiSettings.isMyLocationButtonEnabled = true
+            }
+            uiSettings.isZoomControlsEnabled = true
+        }
+    }
+    fun moverCamaraConZoom(latLng: LatLng, zoom: Float = 17f){
+        mapa.moveCamera(
+            CameraUpdateFactory
+                .newLatLngZoom(latLng, zoom)
+        )
+    }
+
+    fun anadirMarcador(latLang: LatLng, title: String): Marker {
+        return mapa.addMarker(MarkerOptions().position(latLang).title(title))!!
+    }
+
+    fun mostrarSnackbar(texto: String) {
+        val snack = Snackbar.make(
+            findViewById(R.id.main),
+            texto,
+            Snackbar.LENGTH_INDEFINITE
+        )
+
+        snack.show()
     }
 }
